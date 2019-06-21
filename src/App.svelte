2a46de9d7672931fd770b22b4c7a65e80d55c10a
Alexander Baryshnikov <dev@baryshnikov.net>
2019-06-21 20:47:16 +0800

    add update button

--- src/App.svelte
+++ src/App.svelte
@@ -6,84 +6,96 @@
 </div>
 <div class="container grid-xl">
 
-<div class="columns">
-<div class="column col-12">
-    <CreateService label="new service" active="{showCreateService}" on:done="{completeCreating}"></CreateService>
-    <AttachService active="{showAttaching}" on:done="{completeAttaching}"></AttachService>
-    <div class="dropdown">
-        <a href="#" class="btn btn-link dropdown-toggle" tabindex="0">
-            create <i class="icon icon-plus"></i>
-        </a>
+    <div class="columns">
+        <div class="column col-12">
+            <CreateService label="new service" active="{showCreateService}"
+                           on:done="{completeCreating}"></CreateService>
+            <AttachService active="{showAttaching}" on:done="{completeAttaching}"></AttachService>
+            <div class="dropdown">
+                <a href="#" class="btn btn-link dropdown-toggle" tabindex="0">
+                    create <i class="icon icon-plus"></i>
+                </a>
   <!-- menu component -->
-        <ul class="menu">
-            <li class="nav-item">
-                <a href="#" on:click|preventDefault="{()=>showCreateService=true}">new service</a>
-            </li>
-            <li>
-                <a href="#" on:click|preventDefault="{()=>showAttaching=true}">attach</a>
-            </li>
-        </ul>
-    </div>
-    {#if updating}
-        <div class="loading loading-lg"></div>
-    {/if}
-<table class="table">
-    <thead>
-    <tr>
-        <th>name</th>
-        <th>status</th>
-        <th>control</th>
-    </tr>
-    </thead>
-<tbody>
-    {#each services as service}
-    <tr>
-        <td>
-            <a href="{baseURL}monitor/log/{service.name}">{service.name}</a>
-        </td>
-        <td>{service.status}</td>
-    <td>
-        {#if service.status !== 'dead'}
-    <button class="btn tn-action btn-primary"
-            class:loading="{stoppingService[service.name]}"
-            on:click="{()=>stop(service.name)}">
-        <i class="icon icon-stop"></i> stop
-    </button>
-{:else}
-<button class="btn tn-action"
-        class:loading="{startingService[service.name]}"
-        on:click="{()=>start(service.name)}">
-        <i class="icon icon-arrow-up"></i> start
-    </button>
-    {/if}
-        <button class="btn tn-action"
-                class:loading="{restartingService[service.name]}"
-                on:click="{()=>restart(service.name)}">
-            <i class="icon icon-refresh"></i> restart
-        </button>
-    </td>
-    </tr>
-    {/each}
-    </tbody>
-    </table>
-    </div>
+                <ul class="menu">
+                    <li class="nav-item">
+                        <a href="#" on:click|preventDefault="{()=>showCreateService=true}">new service</a>
+                    </li>
+                    <li>
+                        <a href="#" on:click|preventDefault="{()=>showAttaching=true}">attach</a>
+                    </li>
+                </ul>
+            </div>
+            {#if updating}
+                <div class="loading loading-lg"></div>
+            {/if}
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>name</th>
+                    <th>status</th>
+                    <th>control</th>
+                </tr>
+                </thead>
+                <tbody>
+                {#each services as service, i (service.name)}
+                    <tr>
+                        <td>
+                            <a href="{baseURL}monitor/log/{service.name}">{service.name}</a>
+                        </td>
+                        <td>{service.status}</td>
+                    <td>
+                    {#if service.status !== 'dead'}
+                    <button class="btn tn-action btn-primary"
+                            class:disabled="{updating}"
+                            on:click="{()=> stop(service)}">
+                        <i class="icon icon-stop"></i> stop
+                    </button>
+                {:else}
+                <button class="btn tn-action"
+                        class:disabled="{updating}"
+                        on:click="{()=>start(service)}">
+                    <i class="icon icon-arrow-up"></i> start
+                </button>
+                {/if}
+                    <button class="btn tn-action"
+                            class:disabled="{updating}"
+                            on:click="{()=>restart(service)}">
+                        <i class="icon icon-refresh"></i> restart
+                    </button>
+                    <button class="btn tn-action"
+                            class:disabled="{updating}"
+                            on:click="{()=>update(service)}">
+                        <i class="icon icon-upload"></i> update
+                    </button>
+                </td>
+                </tr>
+                {/each}
+                </tbody>
+            </table>
+        </div>
 
     </div>
-    </div>
+</div>
 <script>
     import CreateService from './CreateService.svelte'
     import AttachService from './AttachService.svelte'
 
     const baseURL = process.env.BASE_URL;
+    const refreshInterval = 5000;
 
     let services = [];
-    let startingService = {};
-    let stoppingService = {};
-    let restartingService = {};
     let updating = false;
     let showCreateService = false;
     let showAttaching = false;
 
+    function scheduler() {
+        if (!updating && services.find((srv) => srv.loading)) {
+            updateServices()
+        }
+        setTimeout(scheduler, refreshInterval)
+    }
+
+    //setTimeout(scheduler, refreshInterval);
 
     function updateServices() {
         updating = true;
@@ -102,9 +114,9 @@
         })
     }
 
-    function restart(name) {
-        restartingService[name] = true;
-        fetch(baseURL + "monitor/restart/" + encodeURIComponent(name) + "?nocache=" + Date.now(), {
+    function restart(srv) {
+        updating = true;
+        fetch(baseURL + "monitor/restart/" + encodeURIComponent(srv.name) + "?nocache=" + Date.now(), {
             method: "GET",
             credentials: 'include'
         }).then((data) => {
@@ -113,13 +125,13 @@
             }
             return updateServices();
         }).finally(() => {
-            restartingService[name] = false;
+            updating = false;
         })
     }
 
-    function start(name) {
-        startingService[name] = true;
-        fetch(baseURL + "monitor/run/" + encodeURIComponent(name) + "?nocache=" + Date.now(), {
+    function update(srv) {
+        updating = true;
+        fetch(baseURL + "monitor/update/" + encodeURIComponent(srv.name) + "?nocache=" + Date.now(), {
             method: "GET",
             credentials: 'include'
         }).then((data) => {
@@ -128,14 +140,13 @@
             }
             return updateServices();
         }).finally(() => {
-            startingService[name] = false;
+            updating = false;
         })
     }
 
-    function stop(name) {
-        stoppingService[name] = true;
-
-        fetch(baseURL + "monitor/stop/" + encodeURIComponent(name) + "?nocache=" + Date.now(), {
+    function start(srv) {
+        updating = true;
+        fetch(baseURL + "monitor/run/" + encodeURIComponent(srv.name) + "?nocache=" + Date.now(), {
             method: "GET",
             credentials: 'include'
         }).then((data) => {
@@ -144,7 +155,22 @@
             }
             return updateServices();
         }).finally(() => {
-            stoppingService[name] = false;
+            updating = false;
+        })
+    }
+
+    function stop(srv) {
+        updating = true;
+        fetch(baseURL + "monitor/stop/" + encodeURIComponent(srv.name) + "?nocache=" + Date.now(), {
+            method: "GET",
+            credentials: 'include'
+        }).then((data) => {
+            if (!data.ok) {
+                return;
+            }
+            return updateServices();
+        }).finally(() => {
+            updating = false;
         })
     }
 
